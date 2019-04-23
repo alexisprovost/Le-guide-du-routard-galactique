03f4a99c85a70de044e52e56a2be3386852745bd
1850986 <1850986@li-h014-0t17>
2019-04-23 16:36:52 -0400

    gestionErreur

--- src/ca/qc/bdeb/prog2/leguideduroutardgalactique/Menu.java
+++ src/ca/qc/bdeb/prog2/leguideduroutardgalactique/Menu.java
@@ -6,6 +6,10 @@
 package ca.qc.bdeb.prog2.leguideduroutardgalactique;
 
 import ca.qc.bdeb.prog2.leguideduroutardgalactique.corpsceleste.CorpsCeleste;
+import ca.qc.bdeb.prog2.leguideduroutardgalactique.corpsceleste.Etoile;
+import ca.qc.bdeb.prog2.leguideduroutardgalactique.corpsceleste.Lune;
+import ca.qc.bdeb.prog2.leguideduroutardgalactique.corpsceleste.PlaneteGazeuse;
+import ca.qc.bdeb.prog2.leguideduroutardgalactique.corpsceleste.PlaneteNaine;
 import ca.qc.bdeb.prog2.leguideduroutardgalactique.corpsceleste.PlaneteTellurique;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -118,7 +122,119 @@ public class Menu {
         System.out.println("Quel est son nom?");
     }
 
+    public int gestionErreurChiffre(int min, int max, String chiffre) {
+        boolean boucle = true;
+        String inputChiffre;
+        while (boucle) {
+            try {
+                if (Integer.parseInt(chiffre) < min || Integer.parseInt(chiffre) > max) {
+                    System.out.print("\nErreur chiffre non valide, entrer un chiffre valide: ");
+                    inputChiffre = sc.nextLine();
+                } else if (Integer.parseInt(chiffre) > min || Integer.parseInt(chiffre) < max) {
+                    boucle = false;
+                } else {
+                    System.out.println("Internal Error l.137");
+                }
+            } catch (Exception e) {
+                System.out.print("\nErreur chiffre non valide, entrer un chiffre valide: ");
+                inputChiffre = sc.nextLine();
+                System.out.println(e);
+                //Changer Exception
+            }
+        }
+        return Integer.parseInt(chiffre);
+    }
+
+    public void gestionErreurBoolean(String toBool) {
+        boolean boucle = true;
+        String inputBool = toBool;
+        boolean returnValue = false;
+        while (boucle) {
+            switch (inputBool.toUpperCase()) {
+                case "NON":
+                    returnValue = false;
+                    boucle = false;
+                    break;
+                case "OUI":
+                    returnValue = true;
+                    boucle = false;
+                    break;
+                default:
+                    System.out.print("\nErreur choix non valide, entrer oui ou non: ");
+                    inputBool = sc.nextLine();
+                    break;
+            }
+        }
+    }
+
     public void modifierCorpsCeleste() {
+        System.out.println("\n(1)Supprimer ou (2)modifier: ");
+        String reponse = sc.nextLine();
+        boolean premiereBoucle = true;
+        while (premiereBoucle) {
+            switch (reponse) {
+                case "1":
+                    System.out.println("\nQuel type voulez vous afficher ?\n"
+                            + "1 Planète tellurique\n"
+                            + "2 Planète gazeuse\n"
+                            + "3 Planète naine\n"
+                            + "4 Étoile\n"
+                            + "5 Lune : ");
+
+                    String reponseType = sc.nextLine();
+
+                    switch (reponseType) {
+                        case "1":
+                            for (int i = 0; i < encyclopedie.size(); i++) {
+                                if (encyclopedie.get(i) instanceof PlaneteTellurique) {
+                                    encyclopedie.get(i).affichage();
+                                    System.out.println("\n");
+                                }
+                            }
+                            break;
+                        case "2":
+                            for (int i = 0; i < encyclopedie.size(); i++) {
+                                if (encyclopedie.get(i) instanceof PlaneteGazeuse) {
+                                    encyclopedie.get(i).affichage();
+                                    System.out.println("\n");
+                                }
+                            }
+                            break;
+                        case "3":
+                            for (int i = 0; i < encyclopedie.size(); i++) {
+                                if (encyclopedie.get(i) instanceof PlaneteNaine) {
+                                    encyclopedie.get(i).affichage();
+                                    System.out.println("\n");
+                                }
+                            }
+                            break;
+                        case "4":
+                            for (int i = 0; i < encyclopedie.size(); i++) {
+                                if (encyclopedie.get(i) instanceof Etoile) {
+                                    encyclopedie.get(i).affichage();
+                                    System.out.println("\n");
+                                }
+                            }
+                            break;
+                        case "5":
+                            for (int i = 0; i < encyclopedie.size(); i++) {
+                                if (encyclopedie.get(i) instanceof Lune) {
+                                    encyclopedie.get(i).affichage();
+                                    System.out.println("\n");
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "2":
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 
     public void statistique() {
